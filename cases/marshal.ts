153076b052d1a3f00b6cb7f01bdc431ff5933870
Roman Filippov <rf@romanfilippov.com>
2020-02-05 20:16:12 +0700

    fix: to use validatedPlainToClass

--- cases/marshal.ts
+++ cases/marshal.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import {
   f,
-  validate,
+  validatedPlainToClass,
   PropertyValidator,
   PropertyValidatorError,
 } from '@marcj/marshal';
@@ -72,12 +72,6 @@ export class MarshalCase extends Case implements Case {
   name = 'marshal';
 
   validate() {
-    const errors = validate(DataType, this.data);
-
-    if (errors.length === 0) {
-      return this.data;
-    }
-
-    throw new Error('Invalid');
+    return validatedPlainToClass(DataType, this.data);
   }
 }
