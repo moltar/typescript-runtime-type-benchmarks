8ab172c3e86cceb69fa39ea8f62f41ae4f37c46f
Roman <moltar@moltar.net>
2020-03-18 20:14:44 +0700

    feat: adds purify-ts (#122)
    * feat: adds purify-ts
    
    * docs: adds purify to README
    
    Co-authored-by: Roman Filippov <rf@romanfilippov.com>

--- cases/index.ts
+++ cases/index.ts
@@ -5,6 +5,7 @@ import {
 import { FunvalCase } from "./funval";
 import { IoTsCase } from './io-ts';
 import { MarshalCase } from './marshal';
+import { PurifyCase } from "./purify-ts";
 import { RuntypesCase } from './runtypes';
 import { ToiCase } from './toi';
 import { TsJsonValidatorCase } from './ts-json-validator';
@@ -17,6 +18,7 @@ export const cases = [
   FunvalCase,
   IoTsCase,
   MarshalCase,
+  PurifyCase,
   RuntypesCase,
   ToiCase,
   TsJsonValidatorCase,
