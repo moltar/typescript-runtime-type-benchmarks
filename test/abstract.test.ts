32ae19557468de391283fcbd9d83b3ffd1810317
RA80533 <32469082+RA80533@users.noreply.github.com>
2020-12-28 01:07:18 -0500

    Update abstract.test.ts

--- test/abstract.test.ts
+++ test/abstract.test.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line node/no-unpublished-import
-import { expectTypeOf } from 'expect-type';
 import clone from 'clone';
 import { cases } from '../cases/index';
 import { DATA } from '../data';
