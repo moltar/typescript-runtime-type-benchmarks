383723fb4e1b40d7415c18b60f8ae1f0dc9c72df
Erik Soehnel <eriksoehnel@googlemail.com>
2022-01-10 10:11:46 +0100

    Rebase and add ajv benchmarks

--- cases/ajv.ts
+++ cases/ajv.ts
@@ -1,66 +1,128 @@
 import Ajv from 'ajv';
-import { Case } from './abstract';
-import { Data } from '../data';
+import { addCase, createCase } from '../benchmarks';
 
-const ajv = new Ajv();
-const schema = {
-  $id: 'AjvTest',
-  $schema: 'http://json-schema.org/draft-07/schema#',
-  type: 'object',
-  properties: {
-    number: {
-      type: 'number',
-    },
-    negNumber: {
-      type: 'number',
-    },
-    maxNumber: {
-      type: 'number',
-    },
-    string: {
-      type: 'string',
-    },
-    longString: {
-      type: 'string',
-    },
-    boolean: {
-      type: 'boolean',
-    },
-    deeplyNested: {
-      type: 'object',
-      properties: {
-        foo: {
-          type: 'string',
-        },
-        num: {
-          type: 'number',
-        },
-        bool: {
-          type: 'boolean',
-        },
+createCase('ajv', 'assertLoose', () => {
+  const ajv = new Ajv();
+  const schema = {
+    $id: 'AjvTest',
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    type: 'object',
+    properties: {
+      number: {
+        type: 'number',
       },
-      required: ['foo', 'num', 'bool'],
+      negNumber: {
+        type: 'number',
+      },
+      maxNumber: {
+        type: 'number',
+      },
+      string: {
+        type: 'string',
+      },
+      longString: {
+        type: 'string',
+      },
+      boolean: {
+        type: 'boolean',
+      },
+      deeplyNested: {
+        type: 'object',
+        properties: {
+          foo: {
+            type: 'string',
+          },
+          num: {
+            type: 'number',
+          },
+          bool: {
+            type: 'boolean',
+          },
+        },
+        required: ['foo', 'num', 'bool'],
+      },
     },
-  },
-  required: [
-    'number',
-    'negNumber',
-    'maxNumber',
-    'string',
-    'longString',
-    'boolean',
-    'deeplyNested',
-  ],
-};
-const validate = ajv.compile(schema);
+    required: [
+      'number',
+      'negNumber',
+      'maxNumber',
+      'string',
+      'longString',
+      'boolean',
+      'deeplyNested',
+    ],
+  };
+  const validate = ajv.compile(schema);
 
-export class AjvCase extends Case implements Case {
-  name = 'ajv';
-
-  validate() {
-    if (!validate(this.data)) {
+  return data => {
+    if (!validate(data)) {
       throw new Error(JSON.stringify(ajv.errors, null, 4));
     }
-    return this.data as Data;
-  }
-}
+
+    return true;
+  };
+});
+
+createCase('ajv', 'assertStrict', () => {
+  const ajv = new Ajv();
+  const schema = {
+    $id: 'AjvTest',
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    type: 'object',
+    properties: {
+      number: {
+        type: 'number',
+      },
+      negNumber: {
+        type: 'number',
+      },
+      maxNumber: {
+        type: 'number',
+      },
+      string: {
+        type: 'string',
+      },
+      longString: {
+        type: 'string',
+      },
+      boolean: {
+        type: 'boolean',
+      },
+      deeplyNested: {
+        type: 'object',
+        properties: {
+          foo: {
+            type: 'string',
+          },
+          num: {
+            type: 'number',
+          },
+          bool: {
+            type: 'boolean',
+          },
+        },
+        required: ['foo', 'num', 'bool'],
+        additionalProperties: false,
+      },
+    },
+    required: [
+      'number',
+      'negNumber',
+      'maxNumber',
+      'string',
+      'longString',
+      'boolean',
+      'deeplyNested',
+    ],
+    additionalProperties: false,
+  };
+  const validate = ajv.compile(schema);
+
+  return data => {
+    if (!validate(data)) {
+      throw new Error(JSON.stringify(ajv.errors, null, 4));
+    }
+
+    return true;
+  };
+});
