34951b42fc26d2c8ea303fdfd71987d60edacb0f
McKayla Washburn <mckayla@hey.com>
2023-04-10 22:36:55 -0600

    fix tests, update readme

--- test/benchmarks.test.ts
+++ test/benchmarks.test.ts
@@ -25,6 +25,7 @@ import '../cases/sapphire-shapeshift';
 import '../cases/simple-runtypes';
 import '../cases/sinclair-typebox';
 import '../cases/spectypes';
+import '../cases/succulent';
 import '../cases/superstruct';
 import '../cases/suretype';
 import '../cases/to-typed';
