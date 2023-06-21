af156fd4a8b634bd47441d1007bfa695ac6873ad
Jeongho Nam <samchon.github@gmail.com>
2023-06-21 19:19:50 +0900

    Avoid bug of `vega-lite` for a while
    I found that bug #1082 only occurs in `vega-lite@5` versions. If roll back that version, no problem be occured more. However, `vega-lite@5` occurs type error in `>= TS 4.7` version.
    
    Therefore, chagend `start.sh` file to downgrade the `vega-lite` version and cancel the change after generating `preview.svg` file.

--- start.sh
+++ start.sh
@@ -5,4 +5,6 @@ set -ex
 export NODE_VERSION="${NODE_VERSION:-$(node -v)}"
 
 npm run start
+npm install vega-lite@4.17.0
 npm run start create-preview-svg
+git checkout -- package.json package-lock.json
