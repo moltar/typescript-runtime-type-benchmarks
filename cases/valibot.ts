f68558971f1979f9afa8319a5de35c83f9514673
Naru <naru@naruaway.com>
2023-08-04 03:12:21 +0900

    fix lint

--- cases/valibot.ts
+++ cases/valibot.ts
@@ -22,24 +22,25 @@ createCase('valibot', 'parseSafe', () => {
 });
 
 createCase('valibot', 'parseStrict', () => {
-  const dataType = v.strict(v
-    .object({
+  const dataType = v.strict(
+    v.object({
       number: v.number(),
       negNumber: v.number(),
       maxNumber: v.number(),
       string: v.string(),
       longString: v.string(),
       boolean: v.boolean(),
-      deeplyNested: v.strict(v
-        .object({
+      deeplyNested: v.strict(
+        v.object({
           foo: v.string(),
           num: v.number(),
           bool: v.boolean(),
-        }))
-    }))
+        })
+      ),
+    })
+  );
 
   return data => {
-    return v.safeParse(dataType, data)
+    return v.safeParse(dataType, data);
   };
 });
-
