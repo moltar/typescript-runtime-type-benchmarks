638ed62fa23912811610dfe5bac199b8710fe134
sinclair <haydn.developer@gmail.com>
2023-08-12 07:36:31 +0900

    Update for TypeBox 0.31.0

---
+++ cases/sinclair-typebox-just-in-time.ts
@@ -0,0 +1,23 @@
+import { createCase } from '../benchmarks';
+import { TypeCompiler } from '@sinclair/typebox/compiler';
+import { Loose, Strict } from './sinclair-typebox';
+
+const CheckLoose = TypeCompiler.Compile(Loose);
+const CheckStrict = TypeCompiler.Compile(Strict);
+
+createCase('@sinclair/typebox (just-in-time)', 'assertLoose', () => {
+  return data => {
+    if (!CheckLoose.Check(data)) {
+      throw new Error('validation failure');
+    }
+    return true;
+  };
+});
+createCase('@sinclair/typebox (just-in-time)', 'assertStrict', () => {
+  return data => {
+    if (!CheckStrict.Check(data)) {
+      throw new Error('validation failure');
+    }
+    return true;
+  };
+});
---
+++ cases/sinclair-typebox-value.ts
@@ -0,0 +1,20 @@
+import { createCase } from '../benchmarks';
+import { Value } from '@sinclair/typebox/value';
+import { Loose, Strict } from './sinclair-typebox';
+
+createCase('@sinclair/typebox (value)', 'assertLoose', () => {
+  return data => {
+    if (!Value.Check(Loose, data)) {
+      throw new Error('validation failure');
+    }
+    return true;
+  };
+});
+createCase('@sinclair/typebox (value)', 'assertStrict', () => {
+  return data => {
+    if (!Value.Check(Strict, data)) {
+      throw new Error('validation failure');
+    }
+    return true;
+  };
+});
