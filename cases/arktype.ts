2616f8df50b8671b47fa4256e7bcc53c10089ee3
David Blass <david@arktype.io>
2024-05-15 05:18:58 -0400

    chore: update arktype to 2.0

--- cases/arktype.ts
+++ cases/arktype.ts
@@ -1,7 +1,7 @@
 import { type } from 'arktype';
 import { createCase } from '../benchmarks';
 
-const dataType = type({
+const t = type({
   number: 'number',
   negNumber: 'number',
   maxNumber: 'number',
@@ -15,41 +15,10 @@ const dataType = type({
   },
 });
 
-const dataTypeStrict = type(dataType, { keys: 'strict' });
-
-// TODO: unlike other libraries, arktype does not remove extra keys
-// createCase('arktype', 'parseSafe', () => {
-//   return data => {
-//     const { data: result, problems } = dataType(data);
-//     if (problems) throw new Error(problems.summary);
-//
-//     return result;
-//   };
-// });
-
-createCase('arktype', 'parseStrict', () => {
-  return data => {
-    const { data: result, problems } = dataTypeStrict(data);
-    if (problems) throw new Error(problems.summary);
-
-    return result;
-  };
-});
-
 createCase('arktype', 'assertLoose', () => {
   return data => {
-    const { problems } = dataType(data);
-    if (problems) throw new Error(problems.summary);
-
-    return true;
-  };
-});
-
-createCase('arktype', 'assertStrict', () => {
-  return data => {
-    const { problems } = dataTypeStrict(data);
-    if (problems) throw new Error(problems.summary);
-
-    return true;
+    if (!t.allows(data)) {
+      throw new Error('Invalid');
+    }
   };
 });
