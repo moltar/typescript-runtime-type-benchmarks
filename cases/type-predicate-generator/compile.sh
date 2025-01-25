931dca1b0deda8bd591b41b61cf761ef816bc938
Peter Leonov <gojpeg@gmail.com>
2025-01-25 13:57:30 +0000

    add comments for future us

--- cases/type-predicate-generator/compile.sh
+++ cases/type-predicate-generator/compile.sh
@@ -2,10 +2,15 @@
 
 set -eux -o pipefail
 
+# jump into the current script directory
 cd "$(dirname "$0")"
 
+# remove the old predicate file if exists
 rimraf src/index_guards.ts
-type-predicate-generator ./src/index.ts
+# generate a new predicate file (src/index_guards.ts)
+type-predicate-generator src/index.ts
 
+# remove the old build files if exists
 rimraf build/
+# type check and compile to JS
 tsc -p tsconfig.json
