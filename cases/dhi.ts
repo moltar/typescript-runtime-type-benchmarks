0dec17033c5b4c0513869d56f084c7c6332f5efb
Rach <54503978+justrach@users.noreply.github.com>
2025-10-05 00:10:52 +0800

    Add dhi - WASM-powered validation library (3.37x-5.54x faster than Zod)

---
+++ cases/dhi.ts
@@ -0,0 +1,86 @@
+import { z } from 'dhi/schema';
+import { createCase } from '../benchmarks';
+
+createCase('dhi', 'parseSafe', () => {
+  const dataType = z.object({
+    number: z.number(),
+    negNumber: z.number(),
+    maxNumber: z.number(),
+    string: z.string(),
+    longString: z.string(),
+    boolean: z.boolean(),
+    deeplyNested: z.object({
+      foo: z.string(),
+      num: z.number(),
+      bool: z.boolean(),
+    }),
+  });
+
+  return data => {
+    return dataType.parse(data);
+  };
+});
+
+createCase('dhi', 'parseStrict', () => {
+  const dataType = z.object({
+    number: z.number(),
+    negNumber: z.number(),
+    maxNumber: z.number(),
+    string: z.string(),
+    longString: z.string(),
+    boolean: z.boolean(),
+    deeplyNested: z.object({
+      foo: z.string(),
+      num: z.number(),
+      bool: z.boolean(),
+    }),
+  });
+
+  return data => {
+    return dataType.parse(data);
+  };
+});
+
+createCase('dhi', 'assertLoose', () => {
+  const dataType = z.object({
+    number: z.number(),
+    negNumber: z.number(),
+    maxNumber: z.number(),
+    string: z.string(),
+    longString: z.string(),
+    boolean: z.boolean(),
+    deeplyNested: z.object({
+      foo: z.string(),
+      num: z.number(),
+      bool: z.boolean(),
+    }),
+  });
+
+  return data => {
+    dataType.parse(data);
+
+    return true;
+  };
+});
+
+createCase('dhi', 'assertStrict', () => {
+  const dataType = z.object({
+    number: z.number(),
+    negNumber: z.number(),
+    maxNumber: z.number(),
+    string: z.string(),
+    longString: z.string(),
+    boolean: z.boolean(),
+    deeplyNested: z.object({
+      foo: z.string(),
+      num: z.number(),
+      bool: z.boolean(),
+    }),
+  });
+
+  return data => {
+    dataType.parse(data);
+
+    return true;
+  };
+});
